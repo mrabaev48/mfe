4e7953431bd56e812b70f7344c40815335c8c3e7
mikhail <mikhail.soosaar@fizzdev.ee>
2021-09-16 13:48:46 +0300

    Added dashboard

--- packages/dashboard/config/webpack.common.js
+++ packages/dashboard/config/webpack.common.js
@@ -6,23 +6,21 @@ module.exports = {
     filename: '[name].[contenthash].js',
   },
   resolve: {
-    extensions: ['.js', '.vue']
+    extensions: ['.js', '.vue'],
   },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
-        use: [
-          { loader: 'file-loader' }
-        ],
+        use: [{ loader: 'file-loader' }],
       },
       {
         test: /\.vue$/,
-        use: 'vue-loader'
+        use: 'vue-loader',
       },
       {
         test: /\.scss|\.css$/,
-        use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader']
+        use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.m?js$/,
@@ -30,12 +28,12 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [ '@babel/preset-env'],
+            presets: ['@babel/preset-env'],
             plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
     ],
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [new VueLoaderPlugin()],
 };
